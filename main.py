a307da4f45798eceed7bf7c6c78f6538a746185d
17ronste <45267390+17ronste@users.noreply.github.com>
2018-11-27 10:43:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def monsters(name,use)://selects the monster
     use = r.choice(name)
     return use
 
-monsters = ["troll"] // to add more
+enemies = ["troll"] // to add more
 stuff = 0 //use for anything.
 
